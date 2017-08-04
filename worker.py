786e133d76f2337076a184092092a8eb97af4a98
Bernt Gudmestad <bernt@xspeed.co>
2017-08-04 10:39:27 -0700

    fix hour hours issue

--- worker.py
+++ worker.py
@@ -8,7 +8,7 @@ def run_schedule():
         time.sleep(1)
 
 if __name__ == '__main__':
-    schedule.every(4).hour.do(refresh_trello)
-    schedule.every(8).hour.do(clear_trigger_log_table)
+    schedule.every(4).hours.do(refresh_trello)
+    schedule.every(8).hours.do(clear_trigger_log_table)
     print('starting thread for updating trello')
     run_schedule()
